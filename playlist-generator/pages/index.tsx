ccf67ad2feb24c109533baec2c8f17c8c75a6e91
Jroman5 <jonathan.roman519@gmail.com>
2022-08-20 23:16:38 -0600

    alert displays text submited instead of url variable name

--- playlist-generator/pages/index.tsx
+++ playlist-generator/pages/index.tsx
@@ -3,16 +3,36 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
+//npm run dev or yarn dev or pnpm dev
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <h1>Enter Youtube/Spotify/Apple Music playlist URL!</h1>
-      <label>URL</label>
-      <input type="text" id="fname" name="fname" size={80}/>
-        <br/>
-        <br/>
-        <button type="button">Submit</button> 
+      <form onSubmit= {() =>  {let url = document.querySelector('#url').value
+        if (!url) {
+          alert('Please enter your name.')
+          return ""
+        }
+
+        
+        alert('URLLLLLLLLLLL: ' + url)
+        console.log("URLLLLLLLLLLL: " + url)
+        return url}
+      }>
+      <div>
+        <label htmlFor="url">  URL</label>
+        <input type="text" id="url" name="url" size={80}/>
+      </div>
+      <div>
+        <button type="submit">Submit</button> 
+      </div>
+      </form>
+      <script></script>
+    
     </div>
+  
   )
 }
 
